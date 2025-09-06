55323875fe2ba282262b99e706275db6dcf4fa58
Shanmukh Subrahamanyeswar Thota <163396664+Shannu-1@users.noreply.github.com>
2025-09-06 15:19:59 +0530

    Update App.jsx

--- chat/src/App.jsx
+++ chat/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useRef } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("https://chat-app-duoj.onrender.com/");
+const socket = io("https://chat-app-duoj.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
